253f09c055cfb12f296f5bb8fd633d86e4cfb78b
Scorpion-239 <aum3@njit.edu>
2023-08-02 14:01:02 -0400

    updated playwright

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,22 @@ import { test, expect } from '@playwright/test';
 
 const websiteURL = 'http://localhost:3000'; // Replace with your actual website URL
 
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
 
 
 test('navigation links work correctly', async ({ page }) => {

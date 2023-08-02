a686905439e48974f5d199f58ffdcc1904ce6168
aum3 <85318227+Scorpion-239@users.noreply.github.com>
2023-08-02 14:37:58 -0400

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -20,41 +20,6 @@ test('get started link', async ({ page }) => {
 });
 
 
-test('navigation links work correctly', async ({ page }) => {
-  await page.goto(websiteURL);
 
-  // Click the "Projects" link.
-  await page.click('a[href="#projects"]');
-  // Expect the URL to contain "#projects".
-  await expect(page).toHaveURL(/.*#projects/);
-
-  // Click the "Articles" link.
-  await page.click('a[href="#articles"]');
-  // Expect the URL to contain "#articles".
-  await expect(page).toHaveURL(/.*#articles/);
-
-  // Click the "Contact" link.
-  await page.click('a[href="#contact"]');
-  // Expect the URL to contain "#contact".
-  await expect(page).toHaveURL(/.*#contact/);
-});
-
-
-test('footer links are valid', async ({ page }) => {
-  await page.goto(websiteURL);
-
-  // Click the "Github" link in the footer.
-  await page.click('footer li:nth-child(1) a');
-  // Expect the URL to contain "github.com".
-  await expect(page).toHaveURL(/github.com/);
-
-  // Go back to the homepage.
-  await page.goBack();
-
-  // Click the "Linkedin" link in the footer.
-  await page.click('footer li:nth-child(2) a');
-  // Expect the URL to contain "linkedin.com".
-  await expect(page).toHaveURL(/linkedin.com/);
-});
 
 // Add more tests as needed to cover other interactions and components in your HomePage.
